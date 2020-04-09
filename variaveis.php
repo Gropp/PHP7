f653bfcf5f88af0a003ce0b0b29922718148e65c
Fernando Gropp <fgropp@gmail.com>
2020-04-09 01:10:25 -0300

    Criacao de variaveis

---
+++ variaveis.php
@@ -0,0 +1,15 @@
+<?php
+#linguagem fracamente tipada
+$nome = "variavel string";
+$inteiro = 90;
+$flutuante = 89.4;
+$boolean = true;
+$vetor = [1,2,3,4,5];
+$misto = ["aula",4,60.1,true];
+$matriz = [[2,4],[4,2],[9,5]];
+
+echo $nome;
+echo "<br/>";
+echo $inteiro;
+echo "<br/>";
+var_dump($flutuante);
