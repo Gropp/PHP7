0716c87b89adc2bb42387e6b46f8ec71812ebba1
Fernando Gropp <fgropp@gmail.com>
2020-04-10 18:17:41 -0300

    Sessoes PHP

---
+++ session.php
@@ -0,0 +1,10 @@
+<?php
+//A SESSION NAO VEM ABERTA POR DEFAULT
+//abrindo a sessao
+//cria 
+//php.ini session.autostart (no servidor nao vem habilitado)
+//IMPORTANTE: esse arquivo cria e inicia a sessao criando uma variavel global nessa sessao.
+//arquivos .inc nao sao seguros - includes
+//session_start();
+require_once("config.php");
+$_SESSION["nome"] = "Hcode";
